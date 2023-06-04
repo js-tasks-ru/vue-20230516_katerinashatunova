52077d8a4bafb2f9f818369bc292925a90a0515f
kate-shatunova <katerinashatunova@gmail.com>
2023-06-05 00:50:26 +0500

    solve VueCalculator task

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  data() {
+    return {
+      numberFirst: 0,
+      numberSecond: 0,
+      sign: '',
+    };
+  },
+
+  computed: {
+    result() {
+      switch (this.sign) {
+        case 'sum':
+          return this.numberFirst + this.numberSecond;
+        case 'subtract':
+          return this.numberFirst - this.numberSecond;
+        case 'multiply':
+          return this.numberFirst * this.numberSecond;
+        case 'divide':
+          return this.numberFirst / this.numberSecond;
+        default:
+          return 0;
+      }
+    },
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
