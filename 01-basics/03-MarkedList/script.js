e4bd222e802262d1b79ce6e68ad57c6ed6d6b71a
kate-shatunova <katerinashatunova@gmail.com>
2023-06-06 00:33:16 +0500

    solve MarkedList task

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,34 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const fetchEmails = () =>
+  fetch('https://jsonplaceholder.typicode.com/comments').then((res) => {
+    return res.json();
+  });
+
+const App = defineComponent({
+  data() {
+    return {
+      search: '',
+      emails: null,
+    };
+  },
+
+  computed: {
+    markedEmails() {
+      return this.emails.map((email) => {
+        const marked = this.search ? email.toLowerCase().indexOf(this.search.toLowerCase()) >= 0 : false;
+        return { value: email, marked: marked };
+      });
+    },
+  },
+
+  mounted() {
+    fetchEmails().then((comments) => {
+      this.emails = comments.map((comment) => comment.email);
+    });
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
